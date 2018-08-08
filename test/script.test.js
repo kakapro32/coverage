e25b4607187539cc14c30960a9bdf56056283bb2
duyman <duyman32@gmail.com>
2018-08-08 22:40:51 +0700

    new test

--- test/script.test.js
+++ test/script.test.js
@@ -1,5 +1,12 @@
 var simple = require('../libs/script');
 
-test('should return true', () => {
+test('should return false', () => {
     expect(simple.math(0)).toBe(false);
+});
+test('should return true', () => {
+    expect(simple.math(1)).toBe(true);
+});
+
+test('should return null', () => {
+    expect(simple.math(2)).toBe(null);
 });
